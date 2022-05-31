8fa084528af102e36d20f7b43b020b3e8736ed94
Animesh Karmakar <animesh.pust@gmail.com>
2022-05-31 21:49:16 +0600

    feat: redux set up done.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,19 @@ import "../styles/tailwind.css";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../lib/apollo";
+import {Provider} from "react-redux";
+import {wrapper, store} from "../redux/store"
 
 function MyApp({ Component, pageProps }) {
   return (
-      <ApolloProvider client={client}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ApolloProvider>
+      <Provider store={store}>
+          <ApolloProvider client={client}>
+              <Layout>
+                  <Component {...pageProps} />
+              </Layout>
+          </ApolloProvider>
+      </Provider>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
